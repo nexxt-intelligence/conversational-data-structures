222d7c1ba6ac0100014a08ef29200a6304b54e80
Mike Chen <mikechen@Mikes-MacBook-Pro.local>
2023-01-20 11:48:57 -0800

    added typescript examples

---
+++ src/typescript/index.ts
@@ -0,0 +1,7 @@
+export const Greeter = (name: string) => `Hello ${name}`;
+export const NA_DEFAULT_TEXT = "Not Applicable";
+export enum TESTING {
+  TESTING_ONE = "1",
+  TESTING_TWO = "2",
+  TESTING_THREE = "3",
+}
