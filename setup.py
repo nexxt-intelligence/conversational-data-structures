ebc8cb6d0281fc20f620590b4898dde517b8d67d
Mike Chen <mikechen@Mikes-MacBook-Pro.local>
2023-01-20 11:48:28 -0800

    added python examples

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='inca_interfaces',
-    version='0.0',
+    version='0.0.3',
     license='MIT',
     author="Inca",
     author_email='mike@nexxt.in',
